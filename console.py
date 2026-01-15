554295648a274ee3ccc37453dd04e237dbc5fd8c
Sirius1616 <siriusa1.615@gmail.com>
2026-01-15 01:58:58 +0100

    The command line skeleton

--- console.py
+++ console.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 
+import cmd
+
+
+class ShopNestCommand(cmd.Cmd):
+    prompt = '(shopnest)'
+
+    def do_quit(self, arg):
+        """Cammand that quites the command line"""
+
+        print('Quiting the command line ...')
+        return True
+
+    def do_EOF(self, arg):
+        """Exits the interpreter"""
+
+        print('n\Exiting the CLI...')
