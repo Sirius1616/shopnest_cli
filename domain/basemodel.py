af3a887de300476e626fc438766a89cadf5cdab8
Sirius1616 <siriusa1.615@gmail.com>
2026-01-23 13:25:58 +0100

    Updating the basemodel

--- domain/basemodel.py
+++ domain/basemodel.py
@@ -9,14 +9,32 @@ class BaseModel:
     def __init__(self):
         """Init method to instantiate the classes"""
 
-        self.id = str(uuid.uuid())
-        created_at = datetime.now()
-        updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
 
     def __str__(self):
         """A string representation of how the class will be strown when the string method is called on the class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+
+    def save(self):
+        "Saves an update of any model created"
+        self.updated_at = datetime.now()
+
+
+    def to_dict(self):
+        "Returns the dictionary representation of the class"
+
+        dictn = {}
+
+        dictn['__class__'] = self.__class__.__name__
+
+        for key, value in self.__dict__.items():
+            dictn[key] = value
+
+        return dictn
 
 
 
