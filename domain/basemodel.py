aa434a3289d6eb6d818b09a345c9384d1765a0b3
Sirius1616 <siriusa1.615@gmail.com>
2026-01-23 14:45:15 +0100

    New model

--- domain/basemodel.py
+++ domain/basemodel.py
@@ -6,8 +6,13 @@ from datetime import datetime
 
 class BaseModel:
     """Class that is the base and every other class inherits from"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Init method to instantiate the classes"""
+        
+        if kwargs:
+            for key, value in kwargs:
+                if key != '__class__':
+
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -16,7 +21,7 @@ class BaseModel:
 
     def __str__(self):
         """A string representation of how the class will be strown when the string method is called on the class"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__})"
 
 
     def save(self):
