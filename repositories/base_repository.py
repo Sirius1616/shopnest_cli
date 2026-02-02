383524ac8569b0a781808971bc6d6d9bde2ca1e3
Sirius1616 <siriusa1.615@gmail.com>
2026-02-02 12:01:28 +0100

    Updated base_repository

---
+++ repositories/base_repository.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class BaseRepository:
+    """The storage engine that create way for the serialization of the classes"""
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+
+        return BaseRepository.__objects
+
+
+    def new(self, obj):
+        """Sets in __object the obj with key <obj class name>.id"""
