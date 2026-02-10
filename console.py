c6968134def8c0acee9c18133a418e39d4931463
Sirius1616 <siriusa1.615@gmail.com>
2026-02-10 13:42:37 +0100

    The main CLI

--- console.py
+++ console.py
@@ -6,5 +6,21 @@ import cmd
 
 class ShopNestCommand(cmd.Cmd):
     prompt = '(shopnest) '
+    
 
+    def do_quit(self, arg):
+        """This is the method that exits the console CLI"""
+
+
+        return True
+
+    def do_EOF(self):
+        """Exits the console on the pressing of CTR + D gracefully without an error"""
+
+        return True
+
+
+
+if __name__ == '__main__':
+    ShopNestCommand().cmdloop()
     
