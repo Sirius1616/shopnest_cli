908ae1377789cc4e982b83f6376203eab0d71698
Sirius1616 <siriusa1.615@gmail.com>
2026-02-12 11:46:30 +0100

    Creating a new object

--- console.py
+++ console.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
 import cmd
+from domain.base_model import BaseModel
+from domain.order import Order
+from domain.product import Product
+from domain.user import User
 
+
+
+classes = {'BaseModel': BaseModel, 'Order': Order, 'Product': Product, 'User': User}
 
 
 class ShopNestCommand(cmd.Cmd):
@@ -9,14 +16,24 @@ class ShopNestCommand(cmd.Cmd):
     
 
     def do_quit(self, arg):
-        """This is the method that exits the console CLI"""
+        """Quit command that exits the program"""
 
 
         return True
 
     def do_EOF(self, arg):
         """Exits the console on the pressing of CTR + D gracefully without an error"""
+
         print()
+
+    def do_create(self, arg):
+        """The ultimate creator of classes"""
+        arg_split = arg.split(" ")
+        if len(arg_split) = 1:
+            print(f"** class name missing **")
+        
+
+
         return True
 
 
@@ -25,3 +42,4 @@ if __name__ == '__main__':
     
     ShopNestCommand().cmdloop()
     
+ 
