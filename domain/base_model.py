c24347875e9fb49be8725f7b6b69cfded44597ad
Sirius1616 <siriusa1.615@gmail.com>
2026-02-22 23:50:46 +0100

    Update

--- domain/base_model.py
+++ domain/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
 
     
     def __init__(self, *args, **kwargs):
-        """Init method to instantiate the """
+        """Init method to instantiate the classes"""
         
         if kwargs:
             for key, value in kwargs.items():
