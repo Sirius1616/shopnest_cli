850698f1b5deee716c5f68cb9880861f1277d7e4
Sirius1616 <siriusa1.615@gmail.com>
2026-02-24 10:07:08 +0100

    Update on the storage

--- domain/base_model.py
+++ domain/base_model.py
@@ -35,7 +35,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            
 
 
     def __str__(self):
@@ -47,6 +47,7 @@ class BaseModel:
         "Saves an update of any model created"
         from domain import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
 
