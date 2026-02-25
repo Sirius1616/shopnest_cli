b57d964a9c8f1aea00fe2ad8bbaf7ece109e5a96
Sirius1616 <siriusa1.615@gmail.com>
2026-02-25 09:20:45 +0100

    SqlAlchemy table configurations

--- domain/product.py
+++ domain/product.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 
 from domain.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Product(BaseModel):
     """Class defines a product to be purchased or ordered by a User"""
+
+    __tablename__ = 'products'
     
-    def __init__(self, name=None, price=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        # Type checks if values are provided
-        if name is not None and not isinstance(name, str):
-            raise TypeError(f"name must be a string, got {type(name).__name__}")
-        
-        if price is not None and not isinstance(price, float):
-            raise TypeError(f"price must be a float, got {type(price).__name__}")
-        
-        # Assign attributes
-        self.name = name
-        self.price = price
+    name = Column(String(128), nullable=False)
+    category_id = Column(String(60), ForeignKey('category.id'), nullable=False)
 
